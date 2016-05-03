af3d2b938414f5b29bb06a7ac3e02605e21bf8dc
Shusuke Ishige <stonhair@gmail.com>
2016-05-03 23:53:23 +0900

    Bulletにメソッド追加: 自機の位置へ移動する処理、衝突時の処理

--- bullet.js
+++ bullet.js
@@ -8,6 +8,32 @@ var Bullet = Class.create(Sprite, {
     this.speedY = 0;
   },
 
+  transportTo: function(shooter) {
+    this.x = shooter.x + WIDTH_SPRITE_BASE / 2;
+    this.y = shooter.y + WIDTH_SPRITE_BASE / 2;
+  },
+
+  //衝突候補(衝突する可能性のあるもの)を設定する
+  setTargets: function(targets) {
+    this.targets = targets;
+  },
+
+  affectTo: function(target) {
+    //要検討:ダメージを与える具体的な処理を、敵側と弾丸側のどちらに実装するか
+    //ライフを減らす処理は弾丸側で、外観を変える処理は敵側がよいかもしれない
+    target.damaged();
+  },
+
+  //衝突候補の中から、重なっているものを選び、それらに衝突時の影響を与える
+  affectToTargets: function() {
+    for (var i=0; i< this.targets.length; i++) {
+      if (this.intersect(this.targets[i])) {
+        this.affectTo(this.targets[i]);
+        this.transportTo(this.shooter);
+      }
+    }
+  },
+
   active: function() {
     return (0 - this.width) < this.x;
   },
